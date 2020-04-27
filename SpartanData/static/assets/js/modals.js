731b53fb0ae9793e74349d7a7bdb637524c8a198
srfrankum48 <srf48@case.edu>
2020-04-27 14:01:39 -0700

    Combined modals so that could do a single add graph UI

--- SpartanData/static/assets/js/modals.js
+++ SpartanData/static/assets/js/modals.js
@@ -1,6 +1,7 @@
 
 (function() {
     /* Modal */
+    var modalMaster = document.getElementById('myMasterModal')
     var modalLine = document.getElementById('myLineModal');
     var modalBar = document.getElementById('myBarModal');
     var modalDonut = document.getElementById('myDonutModal');
@@ -8,39 +9,53 @@
 
     /* Launch Button */
     // Get the button that opens the modal
+    var btnMaster = document.getElementById("myMasterBtn")
     var btnLine = document.getElementById("myLineBtn");
     var btnBar = document.getElementById("myBarBtn");
     var btnDonut = document.getElementById("myDonutBtn");
     var btnScatter = document.getElementById("myScatterBtn");
 
     // When the user clicks on the button, open the modal
+    btnMaster.onclick = function() { modalMaster.style.display = "block"}
     btnLine.onclick = function() { modalLine.style.display = "block"; }
     btnBar.onclick = function() { modalBar.style.display = "block"; }
     btnDonut.onclick = function() { modalDonut.style.display = "block"; }
     btnScatter.onclick = function() { modalScatter.style.display = "block"; }
     
     // Get the <span> element that closes the modal
+    var spanMaster = document.getElementById("closeMaster")
     var spanLine = document.getElementById("closeLine");
     var spanBar = document.getElementById("closeBar");
     var spanDonut = document.getElementById("closeDonut");
     var spanScatter = document.getElementById("closeScatter");
     
     // When the user clicks on <span> (x), close the modal
-    spanLine.onclick = function() { modalLine.style.display = "none"; }
-    spanBar.onclick = function() { modalBar.style.display = "none"; }
-    spanDonut.onclick = function() { modalDonut.style.display = "none"; }
-    spanScatter.onclick = function() { modalScatter.style.display = "none"; }
+    spanMaster.onclick = function() { modalMaster.style.display = "none";}
+    spanLine.onclick = function() { modalMaster.style.display = "none";
+                                    modalLine.style.display = "none"; }
+    spanBar.onclick = function() { modalMaster.style.display = "none";
+                                    modalBar.style.display = "none"; }
+    spanDonut.onclick = function() { modalMaster.style.display = "none";
+                                    modalDonut.style.display = "none"; }
+    spanScatter.onclick = function() { modalMaster.style.display = "none";
+                                    modalScatter.style.display = "none"; }
     
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modalLine) {
+            modalMaster.style.display = "none";
             modalLine.style.display = "none";
         } else if (event.target == modalBar) {
+            modalMaster.style.display = "none";
             modalBar.style.display = "none";
         } else if (event.target == modalDonut) {
+            modalMaster.style.display = "none";
             modalDonut.style.display = "none";
         } else if (event.target == modalScatter) {
+            modalMaster.style.display = "none";
             modalScatter.style.display = "none";
+        } else if(event.target == modalMaster){
+            modalMaster.style.display = "none";
         }
     }
     })();
